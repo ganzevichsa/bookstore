6939be2d1af22f79326338abecddd0901d4a0e25
ganzevichsa <74592789+ganzevichsa@users.noreply.github.com>
2024-02-07 14:46:44 +0200

    add comment in index public

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ require_once __DIR__ . '/../core/Router.php';
 
 $router = new Router();
 
+/*-------- WEB ROUTE ----------*/
 $router->get('/', 'BookController@list');
+
+/*-------- API ROUTE ----------*/
+$router->post('/api/authors', 'AuthorController@store'); // Создать нового автора
 
 $router->dispatch();
