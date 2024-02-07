006ae77ab61070d5a0702d45ec47c4b9a61e71aa
ganzevichsa <74592789+ganzevichsa@users.noreply.github.com>
2024-02-07 12:03:00 +0200

    add abstract base controller and interface

---
+++ app/Controllers/BaseController.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Абстрактный класс, основные функции для контроллеров.
+ */
+abstract class BaseController implements ControllerInterface {
+
+    protected $repository;
+
+    /**
+     * Метод-конструктор.
+     */
+    public function __construct($repository) {
+        $this->repository = new $repository();
+    }
+
+    /**
+     * Метод для отображения представления.
+     */
+    public function view($view, $data = []) {
+        require_once __DIR__ . "/../resource/views/{$view}.php";
+    }
+}
---
+++ app/Interface/ControllerInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Интерфейс, определяющий методы для контроллеров.
+ */
+interface ControllerInterface {
+    public function list(); // Метод для отображения списка
+}
